1820d8626df0dffdbc8e2a7d88693eee075bcd4f
Tarik Ibrahimovic <tarikibrahimovic2016@gmail.com>
2023-11-25 01:07:50 +0100

    the architecture done, comments and reply done, showing post excerts done

---
+++ functions.php
@@ -0,0 +1,38 @@
+<?php
+
+function menus(){
+    $locations = array(
+        'primary' => "Desktop Primary Left Sidebar",
+        'footer' => "Footer Menu Items"
+    );
+
+    register_nav_menus($locations);
+}
+
+add_action('init', 'menus');
+
+function theme_support() {
+    add_theme_support('title-tag');
+    add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
+}
+
+add_action('after_setup_theme', 'theme_support');
+
+function wordpressProject_register_styles() {
+    wp_enqueue_style('wordpress-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', array(), '4.4.1', 'all');
+    wp_enqueue_style('wordpress-fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css', array(), '5.13.0', 'all');
+    wp_enqueue_style('wordpress-style', get_template_directory_uri() . "/style.css", array('wordpress-bootstrap'), '1.0', 'all');
+}
+add_action('wp_enqueue_scripts', 'wordpressProject_register_styles');
+
+function wordpressProject_register_scripts() {
+    wp_enqueue_script('wordpress-jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js', array('jquery'), '3.4.1', true);
+    wp_enqueue_script('wordpress-popper',  "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js", array(), '1.16.', true);
+    wp_enqueue_script('wordpress-bootstrap',  "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js", array(), '4.4.1', true);
+    wp_enqueue_script('wordpress-main',  get_template_directory_uri()."/assets/js/main.js", array(), '1.0', true);
+}
+add_action('wp_enqueue_scripts', 'wordpressProject_register_scripts');
+
+?>
+
